d0d7ecb11d9b2179e9d3f1e8e9ee0e93a0466860
emilio barrera <barrera_emilio@hotmail.com>
2018-05-17 23:47:17 +0100

    pattern locator

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿using Lands.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lands.Infrastructure
+{
+    public class InstanceLocator
+    {
+        #region Properties
+
+        public MainViewModel Main { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public InstanceLocator() => Main = new MainViewModel();
+
+        #endregion
+    }
+}
