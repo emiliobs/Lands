8033977dd62c6489c7d779e62cf2df35255f2227
emilio barrera <barrera_emilio@hotmail.com>
2018-05-21 18:44:53 +0100

    Login view Model and Base view model

---
+++ Lands/Lands/ViewModels/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lands.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+}
--- Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -1,27 +1,85 @@
 ﻿namespace Lands.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using System.Windows.Input;
-    public class LoginViewModel
+    using Xamarin.Forms;
+
+
+    public class LoginViewModel : BaseViewModel
     {
         #region Atributtes
-
+        string email;
+        string password;
+        bool isRunning;
+        bool isEnabled;
+      
         #endregion
 
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsRunning
+        {
+            get => isRunning;
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public bool IsRemembered
+        {
+            get;
+            set;
+            
+        }
+        public bool IsEnabled
+        {
+            get => isEnabled;
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         #endregion
 
         #region Constructor
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            IsEnabled = true;
             
         }
         #endregion
@@ -32,16 +90,53 @@
             get => new RelayCommand(Login);
         }
 
+       
+
         #endregion
 
         #region Methods
 
-        private void Login()
+        private async void Login()
         {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error","You must enter an Email..","Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error","You must enter a Password.","Accept");
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            if (Email != "emilio@gmail.com" || Password != "emilio123.")
+            {
+
+                IsRunning = false;
+                IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert("Error", "Email or Password incorreect.", "Accept");
+
+                
+                Password = string.Empty;
+               
+                return;
+            }
+
+            IsRunning = false;
+            IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert("OK", "Fuck yeahhhh", "Accept");
             
         }
 
         #endregion
+
+       
 
 
     }
