37f2d824c3845d1f9469460e0b7cf4d79f9a885c
emilio barrera <barrera_emilio@hotmail.com>
2018-05-22 22:59:37 +0100

    Lan view Model and already the consuming the service

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,10 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lands.ViewModels
+﻿namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Text;
+    using Lands.Serivices;
+    using Models;
+    using Xamarin.Forms;
+
+    public class LandsViewModel : BaseViewModel
     {
+
+        #region Services
+
+        ApiService apiService;
+
+        #endregion
+        #region Atributtes
+
+        private ObservableCollection<Land> lands;
+
+        #endregion
+
+        #region Properties
+
+        //es observableCollection por que la voy a pintar en un listview:
+        public ObservableCollection<Land> Lands
+        {
+            get => lands;
+            set
+            {
+                if (lands != value)
+                {
+                    lands = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Contrustor
+
+        public LandsViewModel()
+        {
+            apiService = new ApiService();
+            LoadLands();
+        }
+
+
+
+        #endregion
+
+        #region Methods
+        private async void LoadLands()
+        {
+            var response = await apiService.GetList<Land>("http://restcountries.eu", "/rest", "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+            }
+
+            var list = (List<Land>)response.Result;
+
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+        #endregion
     }
 }
